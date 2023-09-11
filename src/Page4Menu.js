f559af6a006b2999f550382fcdd68fe323ae0fe2
wezlin <r05525099@ntu.edu.tw>
2023-09-10 20:04:58 -0700

    v5 page4 response ok

--- src/Page4Menu.js
+++ src/Page4Menu.js
@@ -163,8 +163,12 @@ function MenuBody({MenuType}) {
         
         if (response.ok) {
             console.log('Form data sent successfully');
+            const responseData = await response.text();
+            console.log('Backend Response:', responseData);
+            alert('Thank you for your booking; we will get in touch with you');
         } else {
             console.error('Failed to send form data');
+            alert('Error 404');
         }
     };
 
